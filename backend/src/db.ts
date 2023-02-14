ec0e506a485522372b1f1579cf4052b3f5f4b17b
Sammy Au <69769431+samau3@users.noreply.github.com>
2023-02-14 13:59:36 -0800

    Update user model (#3)
    * Update compiler options to es2017 to resolve Object.values error
    
    * Add a sql helper function for sql updating
    
    * Add extra columns for phone and email
    
    * Add update method and include phone and email for user models
    
    * Add non-linux pg uris
    
    * Update update route to include the extra arguments
    
    * Add interface properties that may be used
    
    * Changed user models for getById and getAll to also return phone and email. Fixed user route for updating user data.
    
    ---------
    
    Co-authored-by: Diogo <diogofilipebotelho@gmail.com>

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,12 +2,15 @@
 
 const { Client } = require("pg");
 
-const DB_URI = process.env.NODE_ENV === "test"
-  ? "postgresql://diogobotelho:password@localhost/busk_app_test"
-  : "postgresql://diogobotelho:password@localhost/busk_app";
+const DB_URI =
+  process.env.NODE_ENV === "test"
+    ? "postgresql://diogobotelho:password@localhost/busk_app_test"
+    : "postgresql://diogobotelho:password@localhost/busk_app";
+    // ? "busk_app_test"
+    // : "busk_app";
 
 let db = new Client({
-  connectionString: DB_URI
+  connectionString: DB_URI,
 });
 
 db.connect();

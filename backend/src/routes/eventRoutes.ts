7d2b016b343e2c237cf214e1404e0c008449250a
samau3 <samau415@gmail.com>
2023-02-14 15:13:28 -0800

    Create event model and update routes to utilize it

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -1,72 +1,86 @@
 import express from "express";
 
 import db from "../db";
+import { Event } from "../models/event";
 const router = express.Router();
 
 /** Get events: [event, event, event] */
-router.get("/", async function (req: express.Request, res: express.Response, next:express.NextFunction) {
-  const results = await db.query(
-    `SELECT id, busker_id, title, type
-           FROM events`);
-  const events = results.rows;
-  return res.json({ events });
-});
+router.get(
+  "/",
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const events = await Event.getAll();
+    return res.json(events);
+  }
+);
 
 /** Get event, return event */
-router.get("/:id", async function (req: express.Request, res: express.Response, next:express.NextFunction) {
-  const results = await db.query(
-    `SELECT id,busker_id, title, type
-         FROM events
-         WHERE id = $1`,
-    [req.params.id],
-  );
-  const event = results.rows[0];
-  if (!event) return res.status(404);
-  return res.json({ event });
-});
+router.get(
+  "/:id",
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const { id } = req.params;
+
+    const event = await Event.getById(+id);
+    return res.json({ event });
+  }
+);
 
 /** Create new event, return event */
 
-router.post("/", async function (req: express.Request, res: express.Response, next:express.NextFunction) {
-  const { busker_id, title, type } = req.body;
+router.post(
+  "/",
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const { buskerId, title, type } = req.body;
 
-  const result = await db.query(
-    `INSERT INTO events (busker_id, title, type)
-             VALUES ($1, $2, $3)
-             RETURNING id, busker_id, title, type`,
-    [busker_id, title, type],
-  );
-  const event = result.rows[0];
-  return res.status(201).json({ event });
-});
+    const event = await Event.create(buskerId, title, type);
+    return res.status(201).json({ event });
+  }
+);
 
 /** Update event, returning event */
 
-router.patch("/:id", async function (req: express.Request, res: express.Response, next:express.NextFunction) {
-  const { title, type } = req.body;
+router.patch(
+  "/:id",
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const { id } = req.params;
 
-  const result = await db.query(
-    `UPDATE events
-             SET title=$1,
-                 type=$2
-             WHERE id = $3
-             RETURNING id, busker_id, title, type`,
-    [title, type, req.params.id],
-  );
-  // if(!result.rows[0]) throw new NotFoundError();
-  const event = result.rows[0];
-  if (!event) return res.status(404);
-  return res.json({ event });
-});
+    const event = await Event.update(+id, req.body);
+    // if(!result.rows[0]) throw new NotFoundError();
+
+    if (!event) return res.status(404);
+    return res.json({ event });
+  }
+);
 
 /** Delete event, returning {message: "Deleted"} */
 
-router.delete("/:id", async function (req: express.Request, res: express.Response, next:express.NextFunction) {
-  await db.query(
-    "DELETE FROM events WHERE id = $1",
-    [req.params.id],
-  );
-  return res.json({ message: "event deleted" });
-});
+router.delete(
+  "/:id",
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const { id } = req.params;
+
+    await Event.remove(+id);
+    return res.json({ message: "event deleted" });
+  }
+);
 
 export { router };

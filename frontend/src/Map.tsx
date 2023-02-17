c7f5b1876d7235b38bdf03115cfe98644d774df7
Diogo <diogofilipebotelho@gmail.com>
2023-02-17 17:51:30 -0500

    Started feature to add a marker.
    Created LocationMarker.tsx
    Updated Map.tsx to load LocationMarker.

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -1,5 +1,6 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import "./Map.css";
+import { LocationMarker } from "./LocationMarker"
 
 export function Map() {
   return (
@@ -8,11 +9,12 @@ export function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[40.7826, -73.9656]}>
+        {/* <Marker position={[40.7826, -73.9656]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
+        </Marker> */}
+        <LocationMarker />
       </MapContainer>
   );
 }

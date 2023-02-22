3ff438cabe9e7b9065338affdadab29b3f8c04d5
samau3 <samau415@gmail.com>
2023-02-22 13:06:34 -0800

    Resolve type error for Map prop param

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -1,15 +1,24 @@
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
 import "./Map.css";
-import { LocationMarker } from "./LocationMarker"
+import { LocationMarker } from "./LocationMarker";
 
-export function Map({isAddingEvent}:any) { //Can't get this to work with type:boolean? 
+interface MapParams {
+  isAddingEvent: boolean;
+}
+
+export function Map({ isAddingEvent }: MapParams) {
   return (
-      <MapContainer className="map" center={[40.7826, -73.9656]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        { isAddingEvent ? <LocationMarker /> : undefined }
-      </MapContainer>
+    <MapContainer
+      className="map"
+      center={[40.7826, -73.9656]}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {isAddingEvent ? <LocationMarker /> : undefined}
+    </MapContainer>
   );
 }

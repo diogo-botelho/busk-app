9fd8001b8b337d4d8a02454c5fe1ba248ff4fd0c
Diogo <diogofilipebotelho@gmail.com>
2023-02-28 10:20:34 -0500

    Formatted EventMarker.

--- frontend/src/EventMarker.tsx
+++ frontend/src/EventMarker.tsx
@@ -7,23 +7,22 @@ interface LocationMarkerParams {
 }
 
 export function EventMarker({ key, event }: LocationMarkerParams) {
-  
-  const {title, type} = event;
+  const { title, type } = event;
   const { lat, lng } = event.coordinates;
   // const lat = event.coordinates.lat;
   // const lng = event.coordinates.lng;
 
   const position = [];
   if (lat && lng) {
-    position.push( lat);
+    position.push(lat);
     position.push(lng);
   }
 
   return lat && lng ? (
     <Marker key={key} position={[lat, lng]}>
       <Popup>
-      Title: {title} <br />
-      Type: {type} <br />
+        Title: {title} <br />
+        Type: {type} <br />
       </Popup>
     </Marker>
   ) : null;

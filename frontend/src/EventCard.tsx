336f7666704ab013e64eebc8e61f9b0dbfbd47fc
Diogo <diogofilipebotelho@gmail.com>
2023-03-16 14:50:45 -0400

    Added EventCard and EventListCard.

---
+++ frontend/src/EventCard.tsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import "./User.css";
+import { UserContext } from "./UserContext";
+import { Container } from "react-bootstrap";
+
+function EventCard() {
+}
+
+export default EventCard;

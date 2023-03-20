2b788e60f465d590ebf61377422bf3b1adc68a2c
Diogo <diogofilipebotelho@gmail.com>
2023-03-20 18:27:46 -0400

    Added route for events/:id

--- frontend/src/routes-nav/AllRoutes.tsx
+++ frontend/src/routes-nav/AllRoutes.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
 import LoginForm from "../auth/LoginForm";
-import SignupForm from "../auth/SignupForm"
+import SignupForm from "../auth/SignupForm";
 
 import EventList from "../events/EventList";
+import { EventDetail } from "../events/EventDetail";
 
 import Home from "../homepage/Home";
 
 import { LoginFormData } from "../interfaces/LoginFormData";
-import { SignupFormData } from "../interfaces/SignupFormData"
+import { SignupFormData } from "../interfaces/SignupFormData";
 
 import UserList from "../users/UserList";
 import User from "../users/User";
@@ -32,13 +33,10 @@ function AllRoutes({ login, signup }: AllRoutesParams) {
       <Route path="/users" element={<UserList />} />
       <Route path="/users/:username" element={<User />} />
       <Route path="/events" element={<EventList />} />
-      {/* <Route path="/events/:id" element={<Event />} /> */}
+      <Route path="/events/:id" element={<EventDetail />} />
       <Route path="/login" element={<LoginForm login={login} />} />
-      <Route
-        path="/signup"
-        element={<SignupForm signup={signup} />}
-      />
-      <Route path="/*" element={<Home />} /> {/*Works but is it best practice? */}
+      <Route path="/signup" element={<SignupForm signup={signup} />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }

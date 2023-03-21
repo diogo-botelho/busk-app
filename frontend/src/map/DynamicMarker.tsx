be8692a9177062e19b4f0795d670da5e05eac192
Diogo <diogofilipebotelho@gmail.com>
2023-03-21 15:48:01 -0400

    Added position state.
    Changed type of updateNewCoordinates from any to NewCoordinatesContextInterface.
    Updated return statement.
    Cleaned up unused code.

--- frontend/src/map/DynamicMarker.tsx
+++ frontend/src/map/DynamicMarker.tsx
@@ -1,10 +1,14 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Marker, useMapEvents } from "react-leaflet";
+import { LatLngExpression, latLng } from "leaflet";
 
-import { NewCoordinatesContext } from "./NewCoordinatesContext";
+import {
+  NewCoordinatesContext,
+  NewCoordinatesContextInterface,
+} from "./NewCoordinatesContext";
 
 /** Dynamic Marker Component to add/update events.
- * 
+ *
  * On click, renders React Leaflet Marker component, tracks the marker's
  * coordinates on the map and updates newCoordinates context.
  *
@@ -20,17 +24,18 @@ import { NewCoordinatesContext } from "./NewCoordinatesContext";
  */
 
 export function DynamicMarker() {
-  let { newCoordinates, updateNewCoordinates } = useContext<any | undefined>(
+  let { updateNewCoordinates } = useContext<NewCoordinatesContextInterface>(
     NewCoordinatesContext
   );
+  const [position, setPositon] = useState<LatLngExpression | null>(null);
 
   useMapEvents({
     click(e) {
-      updateNewCoordinates(e.latlng);
+      const coordinates = latLng({ lat: e.latlng.lat, lng: e.latlng.lng });
+      updateNewCoordinates(coordinates);
+      setPositon(e.latlng);
     },
   });
 
-  return newCoordinates === undefined ? null : (
-    <Marker position={newCoordinates}></Marker>
-  );
+  return position === null ? null : <Marker position={position}></Marker>;
 }

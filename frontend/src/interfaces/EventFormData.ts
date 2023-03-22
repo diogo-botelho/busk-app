4637bf9ae812de840c09043399df5908ef3afbf4
Diogo <diogofilipebotelho@gmail.com>
2023-03-22 07:02:25 -0400

    Fixed interface naming

--- frontend/src/interfaces/EventFormData.ts
+++ frontend/src/interfaces/EventFormData.ts
@@ -1,7 +1,10 @@
-export interface eventFormData {
+import { NewCoordinatesContextInterface } from "../map/NewCoordinatesContext";
+
+export interface EventFormData {
   title: string;
   type: string;
   date: any;
   startTime: any;
   endTime: any;
-};
+  coordinates: NewCoordinatesContextInterface["newCoordinates"];
+}

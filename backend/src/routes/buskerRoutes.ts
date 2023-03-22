efd6c405291abd9fc453d049553b9d28fdfa15ba
Diogo <diogofilipebotelho@gmail.com>
2023-03-22 08:49:46 -0400

    Created Busker Routes

--- backend/src/routes/buskerRoutes.ts
+++ backend/src/routes/buskerRoutes.ts
@@ -1,32 +1,111 @@
 import express from "express";
 
 import db from "../db";
+/** Routes for buskers. */
+
+import { Busker } from "../models/busker";
+import { ensureCorrectUserOrAdmin, ensureAdmin } from "../middleware/auth";
+import { BuskerData } from "../interfaces/BuskerData";
+
 const router = express.Router();
 
-/** GET /events: get list of buskers */
+
+/** GET / => { buskers: [ {buskerId, buskerName, category, descritpion }, ... ] }
+ *
+ * Returns list of all buskers.
+ *
+ * Authorization required: admin
+ **/
+
 router.get(
   "/",
-  function (
+  ensureAdmin,
+  async function (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
   ) {
-    return res.send("Buskers will go here");
-    // return res.json(db.Busker.all());
+    try {
+      const buskers = await Busker.getAll();
+      return res.json(buskers);
+    } catch (err) {
+      return next(err);
+    }
   }
 );
 
-// /** DELETE /events/[id]: delete event, return {message: Deleted} */
-// router.delete(
-//   "/:id",
-//   function (
-//     req: express.Request,
-//     res: express.Response,
-//     next: express.NextFunction
-//   ) {
-//     db.Busker.delete(req.params.id);
-//     return res.json({ message: "Deleted" });
-//   }
-// );
+/** GET /[buskerId] => { user }
+ *
+ * Returns { buskerId, buskerName, category, description }
+ *
+ * Authorization required: admin or same user-as-:id
+ **/
+router.get(
+  "/:buskerId",
+  ensureCorrectUserOrAdmin,
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const { buskerId } = req.params;
+      const busker = await Busker.get(+buskerId);
+      return res.json(busker);
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** PATCH /[buskerId] { busker } => { busker }
+ *
+ * Data can include:
+ *   { buskerName, category, description }
+ *
+ * Returns { buskerId, buskerName, category, description }
+ *
+ * Authorization required: admin or same-user-as-:username
+ **/
+
+router.patch(
+  "/:buskerId",
+  ensureCorrectUserOrAdmin,
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    try {
+      const { buskerId } = req.params;
+
+      const busker = await Busker.update(+buskerId, req.body);
+
+      return res.json({ buskerId });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+
+/** DELETE /[buskerId]  =>  { deleted: buskerId }
+ *
+ * Authorization required: admin or same-user-as-:username
+ **/
+router.delete(
+  "/:buskerId",
+  ensureCorrectUserOrAdmin,
+  async function (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    const { buskerId } = req.params;
+
+    await Busker.remove(+buskerId);
+    return res.json({ message: `User ${buskerId} deleted.` });
+  }
+);
 
 export { router };

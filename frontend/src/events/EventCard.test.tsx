ef75b689b5d9205f32dfea955bb785a4e1acae51
Diogo <diogofilipebotelho@gmail.com>
2023-03-27 18:23:23 -0400

    Started tests for frontend.
    Tests added:
    - App - failing
    - AllRoutes - failing
    - LoginForm - passing
    - SignupForm
    - AddEventForm
    - UpdateEventForm
    - EventCard
    - Home
    - Navbar

---
+++ frontend/src/events/EventCard.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { EventCard } from "./EventCard";
+
+const demoEvent = {
+  id: 1,
+  buskerId: 1,
+  title: "demo title",
+  type: "demo type",
+  coordinates: { lat: 1, lng: 1 },
+};
+
+it("renders without crashing", function () {
+  render(<EventCard event={demoEvent} />);
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<EventCard event={demoEvent} />);
+  expect(asFragment()).toMatchSnapshot();
+});

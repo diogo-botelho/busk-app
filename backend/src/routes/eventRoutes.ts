5f3c8d56b59a3b59b3eb84d65c4757d3c804ef33
Diogo <diogofilipebotelho@gmail.com>
2023-04-05 18:49:47 -0400

    Refactored event Routes.

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -57,12 +57,12 @@ router.post(
     next: express.NextFunction
   ) {
     try {
-      const validator = jsonschema.validate(req.body, eventNewSchema);
+      const validator = jsonschema.validate(req.body.eventData, eventNewSchema);
       if (!validator.valid) {
         const errs = validator.errors.map((e) => e.stack);
         throw new BadRequestError(...errs);
       }
-      const eventDetails = req.body;
+      const eventDetails = req.body.eventData;
       const event = await Event.create(eventDetails);
 
       return res.status(201).json({ event });
@@ -85,14 +85,17 @@ router.patch(
     next: express.NextFunction
   ) {
     try {
-      const validator = jsonschema.validate(req.body, eventUpdateSchema);
+      const validator = jsonschema.validate(
+        req.body.updateData,
+        eventUpdateSchema
+      );
       if (!validator.valid) {
         const errs = validator.errors.map((e) => e.stack);
         throw new BadRequestError(...errs);
       }
       const { id } = req.params;
 
-      const event = await Event.update(+id, req.body);
+      const event = await Event.update(+id, req.body.updateData);
 
       return res.status(201).json({ event });
     } catch (err) {
@@ -113,14 +116,10 @@ router.delete(
     res: express.Response,
     next: express.NextFunction
   ) {
-    try {
-      const { id } = req.params;
+    const { id } = req.params;
+    await Event.remove(+id);
 
-      await Event.remove(+id);
-      return res.json({ message: "event deleted" });
-    } catch (err) {
-      return next(err);
-    }
+    return res.json({ message: "event deleted" });
   }
 );
 

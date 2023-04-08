df76cf7031b0f7f794708eaf137c774a2bd3f9fc
samau3 <samau415@gmail.com>
2023-04-07 17:55:27 -0700

    Remove leftover code

--- backend/src/middleware/auth.test.ts
+++ backend/src/middleware/auth.test.ts
@@ -10,23 +10,10 @@ import {
 } from "./auth";
 import { Request, Response, NextFunction } from "express";
 
-import {
-  commonBeforeAll,
-  commonBeforeEach,
-  commonAfterEach,
-  commonAfterAll,
-} from "./_testCommon";
-
-beforeAll(commonBeforeAll);
-beforeEach(commonBeforeEach);
-afterEach(commonAfterEach);
-afterAll(commonAfterAll);
-
 import { SECRET_KEY } from "../config";
 
 const testJwt = jwt.sign({ id: 1, isAdmin: false }, SECRET_KEY);
 const badJwt = jwt.sign({ id: 1, isAdmin: false }, "wrong");
-
 
 describe("authenticateJWT", function () {
   test("works: via header", function () {

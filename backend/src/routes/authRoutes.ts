34d74201942217d021e291b9362f7140a96dcc4c
Diogo <diogofilipebotelho@gmail.com>
2023-04-21 18:15:29 -0400

    Modified signup route to support busker registration.
    Included buskerNewSchema validation.
    Probably good to think about refactoring the req.body deconstruction.

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -2,12 +2,19 @@ import express from "express";
 import jsonschema from "jsonschema";
 
 import { User } from "../models/user";
-export const router = express.Router();
+import { Busker } from "../models/busker";
+
 import { UserData } from "../interfaces/UserData";
+import { createResLocalsBuskers } from "../helpers/resLocals";
+
 import { createToken } from "../helpers/tokens";
 import { BadRequestError } from "../expressError";
 import userLoginSchema from "../schemas/userLogin.json";
 import userSignupSchema from "../schemas/userSignup.json";
+import buskerNewSchema from "../schemas/buskerNew.json";
+// import { ensureCorrectUserOrAdmin } from "src/middleware/auth";
+
+export const router = express.Router();
 
 /** Logs in a user, return user */
 router.post(
@@ -44,17 +51,55 @@ router.post(
     next: express.NextFunction
   ) {
     try {
-      const validator = jsonschema.validate(req.body, userSignupSchema);
-      if (!validator.valid) {
+      const {
+        email,
+        password,
+        firstName,
+        lastName,
+        phone,
+        buskerCheckmark,
+        buskerName,
+        category,
+        description,
+      } = req.body;
+
+      const userSignupData = {
+        email,
+        password,
+        firstName,
+        lastName,
+        phone,
+      };
+
+      const buskerRegistrationData = {
+        buskerName,
+        category,
+        description,
+      };
+
+      const validator = jsonschema.validate(userSignupData, userSignupSchema);
+      const buskerValidator = jsonschema.validate(
+        buskerRegistrationData,
+        buskerNewSchema
+      );
+
+      if (!validator.valid || (buskerCheckmark && !buskerValidator.valid)) {
         const errs = validator.errors.map((e) => e.stack);
+        errs.concat(buskerValidator.errors.map((e) => e.stack));
         throw new BadRequestError(...errs);
       }
 
-      const newUserData: UserData = req.body;
-
+      const newUserData: UserData = userSignupData;
       const newUser = await User.signup({ ...newUserData, isAdmin: false });
       delete newUser.isAdmin;
-      const token = createToken(newUser.id, false);
+      const userId = newUser.id;
+      const token = createToken(userId, false);
+
+      if (buskerCheckmark) {
+        const busker = await Busker.register(userId, buskerRegistrationData);
+        res.locals.buskers = await createResLocalsBuskers(userId, res);
+      }
+      
       return res.status(201).json({ token });
     } catch (error) {
       next(error);

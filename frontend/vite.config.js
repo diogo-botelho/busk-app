b7f45ecabd83479fe244d59317a3021fde53aec6
samau3 <samau415@gmail.com>
2023-10-03 17:19:43 -0700

    Add vite setting to output to build folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import macrosPlugin from "vite-plugin-babel-macros";
 
 export default defineConfig(() => {
   return {
+    // since deployment hasn't been updated to look for the dist folder, add this config to prevent breaking from previous CRA setup
+    build: {
+      outDir: 'build',
+    },
     define: {
       'process.env': {},
     },

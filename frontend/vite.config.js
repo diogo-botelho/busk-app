ffa9d755f54fe4c0aac1e3b7472e942abe701958
samau3 <samau415@gmail.com>
2023-10-09 16:53:43 -0700

    Add Typescript checking to Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import macrosPlugin from "vite-plugin-babel-macros";
+import checker from 'vite-plugin-checker'
 
 export default defineConfig(() => {
   return {
@@ -14,6 +15,7 @@ export default defineConfig(() => {
     plugins: [
       react(),
       macrosPlugin(),
+      checker({ typescript: true })
     ],
     server: {
       open: true,

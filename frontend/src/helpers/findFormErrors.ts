22ef13eb796e39c652a3f2da28f57645a5c9e140
samau3 <samau415@gmail.com>
2023-11-10 15:27:01 -0800

    Add form error helper fn

---
+++ frontend/src/helpers/findFormErrors.ts
@@ -0,0 +1,17 @@
+import { EventFormData, EventFormErrors } from "../interfaces/EventFormData";
+
+export default function findFormErrors(eventData: EventFormData) {
+  const newErrors: EventFormErrors = {};
+  if (!eventData.title || eventData.title === "")
+    newErrors.title = "Please include a title";
+  if (!eventData.type || eventData.type === "")
+    newErrors.type = "Please include a type";
+  if (!eventData.date || eventData.date === "")
+    newErrors.date = "Please include a date";
+  if (!eventData.startTime || eventData.startTime === "")
+    newErrors.startTime = "Please include a start time";
+  if (!eventData.endTime || eventData.endTime === "")
+    newErrors.endTime = "Please include a end time";
+
+  return newErrors;
+}
